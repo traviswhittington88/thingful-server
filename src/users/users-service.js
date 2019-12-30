1a45db14826642dad739b99f04a3a82a2b3492ad
traviswhittington88 <traviswhittington@ymail.com>
2019-12-29 18:42:14 -0600

    added serializeUser service function

--- src/users/users-service.js
+++ src/users/users-service.js
@@ -1,3 +1,5 @@
+const xss = require('xss')
+
 const REGEX_UPPER_LOWER_NUMBER_SPECIAL = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&])[\S]+/
 
 const UsersService = {
@@ -31,6 +33,15 @@ const UsersService = {
       .into('thingful_users')
       .returning('*')
       .then(([user]) => user)
+  },
+  serializeUser(user) {
+    return {
+      id: xss(user.id),
+      user_name: xss(user.user_name),
+      full_name: xss(user.full_name),
+      nickname: xss(user.nickname),
+      date_created: new Date(user.date_created),
+    }
   }
 }
 
